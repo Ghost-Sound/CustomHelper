971bb3527ca74bd50cb88cf0b62691a6d8df6efe
Yehor Kadeniuk <91326774+newmasterSG@users.noreply.github.com>
2024-01-17 00:05:37 +0200

    Add jwt attribute

---
+++ CustomHelper/CustomHelper/Authentication/Attributes/JwtAuthorizeAttribute.cs
@@ -0,0 +1,100 @@
+﻿using CustomHelper.Authentication.Interfaces;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomHelper.Authentication.Attributes
+{
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, AllowMultiple = true)]
+    public class JwtAuthorizeAttribute : Attribute, IAsyncAuthorizationFilter
+    {
+        private readonly IConfiguration _configuration;
+        private readonly ISignInKeys _signInKeys;
+        public string[] _policies { get; }
+
+        public JwtAuthorizeAttribute(
+            string[] policies,
+            IConfiguration configuration,
+            ISignInKeys signInKeys)
+        {
+            _policies = policies;
+            _configuration = configuration;
+            _signInKeys = signInKeys;
+        }
+
+        public async Task OnAuthorizationAsync(AuthorizationFilterContext context)
+        {
+            if (!context.HttpContext.User.Identity.IsAuthenticated)
+            {
+                context.Result = new UnauthorizedResult();
+                return;
+            }
+
+            if (_policies.Any())
+            {
+                var authService = context.HttpContext.RequestServices.GetService(typeof(IAuthorizationService)) as IAuthorizationService;
+
+                var isAuthorized = false;
+
+                foreach (var policy in _policies)
+                {
+                    var authorizationResult = await authService.AuthorizeAsync(context.HttpContext.User, context, policy);
+                    if (authorizationResult.Succeeded)
+                    {
+                        isAuthorized = true;
+                        break;
+                    }
+                }
+
+                if (!isAuthorized)
+                {
+                    context.Result = new ForbidResult();
+                    return;
+                }
+            }
+
+            var jwtToken = context.HttpContext.Request.Headers["Authorization"].ToString().Replace("Bearer ", "");
+
+            var issuer = _configuration["Jwt:Issuer"];
+            var audience = _configuration["Jwt:Audience"];
+
+            if (string.IsNullOrEmpty(jwtToken))
+            {
+                context.Result = new UnauthorizedResult();
+                return;
+            }
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var validationParameters = new TokenValidationParameters
+            {
+                ValidateIssuerSigningKey = true,
+                IssuerSigningKey = (SecurityKey)await _signInKeys.GetSigninKeys(audience),
+                ValidateIssuer = !string.IsNullOrEmpty(issuer),
+                ValidIssuer = issuer,
+                ValidateAudience = !string.IsNullOrEmpty(audience),
+                ValidAudience = audience,
+                ValidateLifetime = true,
+            };
+
+            try
+            {
+                var principal = tokenHandler.ValidateToken(jwtToken, validationParameters, out _);
+
+                return;
+            }
+            catch (System.Exception)
+            {
+                context.Result = new UnauthorizedResult();
+                return;
+            }
+        }
+    }
+}
