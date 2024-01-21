abfcee6c7710a535a555006a7e320995f43c4719
Yehor Kadeniuk <91326774+newmasterSG@users.noreply.github.com>
2024-01-21 11:05:33 +0200

    Adding middleware and one more exeption

---
+++ CustomHelper/CustomHelper/Exception/DataNotModifiedException.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomHelper.Exception
+{
+    [Serializable]
+    public class DataNotModifiedException : CustomException
+    {
+        public string AdditionalMessage { get; set; }
+
+        public DataNotModifiedException() : base("The data have not been changed.")
+        {
+            AdditionalMessage = string.Empty;
+        }
+
+        public DataNotModifiedException(string additionalMessage) : base("The data have not been changed.")
+        {
+            AdditionalMessage = additionalMessage;
+        }
+
+        public DataNotModifiedException(string message, string additionalMessage) : base(message)
+        {
+            AdditionalMessage = additionalMessage;
+        }
+
+        public DataNotModifiedException(string message, System.Exception innerException) : base(message, innerException)
+        {
+            AdditionalMessage = string.Empty;
+        }
+
+        public DataNotModifiedException(string message, string additionalMessage, System.Exception innerException) : base(message, innerException)
+        {
+            AdditionalMessage = additionalMessage;
+        }
+    }
+}
---
+++ CustomHelper/CustomHelper/Middlewares/ExceptionHandlingMiddleware.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomHelper.Middlewares
+{
+    public class ExceptionHandlingMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly ILogger _logger;
+
+        public ExceptionHandlingMiddleware(RequestDelegate next, ILogger logger)
+        {
+            _next = next;
+            _logger = logger;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            try
+            {
+                await _next(context);
+            }
+            catch (System.Exception ex)
+            {
+                _logger.LogError($"An error occurred: {ex}");
+
+                context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+                await context.Response.WriteAsync($"Internal Server Error {ex.Message}");
+            }
+        }
+    }
+}
