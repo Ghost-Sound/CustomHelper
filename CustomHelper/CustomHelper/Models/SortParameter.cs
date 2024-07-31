700377b884214437b73dca8fc8567ad028f6911f
Yehor Kadeniuk <91326774+newmasterSG@users.noreply.github.com>
2024-07-31 16:42:29 +0300

    SortParam

---
+++ CustomHelper/CustomHelper/Models/SortParameter.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomHelper.Models
+{
+    public class SortParameter
+    {
+        public string PropertyName { get; set; }
+        public bool IsDescending { get; set; }
+    }
+}
