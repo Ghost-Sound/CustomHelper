8d51e66e7d6617bdadd7c2e864c468602271e5a5
Yehor Kadeniuk <91326774+newmasterSG@users.noreply.github.com>
2024-08-04 21:31:00 +0300

    add test helpers for grpc

---
+++ CustomHelper/CustomHelper/GRPC/TestHelpers/TestAsyncStreamReader.cs
@@ -0,0 +1,43 @@
+﻿using Grpc.Core;
+using System.Threading.Channels;
+
+namespace CustomHelper.GRPC.TestHelpers
+{
+    public class TestAsyncStreamReader<T>(ServerCallContext serverCallContext) : IAsyncStreamReader<T> where T : class
+    {
+        private readonly Channel<T> _channel = Channel.CreateUnbounded<T>();
+        private readonly ServerCallContext _serverCallContext = serverCallContext;
+
+        public T Current { get; private set; } = null!;
+
+        public void AddMessage(T message)
+        {
+            if (!_channel.Writer.TryWrite(message))
+            {
+                throw new InvalidOperationException("Unable to write message.");
+            }
+        }
+
+        public void Complete()
+        {
+            _channel.Writer.Complete();
+        }
+
+        public async Task<bool> MoveNext(CancellationToken cancellationToken)
+        {
+            _serverCallContext.CancellationToken.ThrowIfCancellationRequested();
+
+            if (await _channel.Reader.WaitToReadAsync(cancellationToken) &&
+                _channel.Reader.TryRead(out var message))
+            {
+                Current = message;
+                return true;
+            }
+            else
+            {
+                Current = null!;
+                return false;
+            }
+        }
+    }
+}
---
+++ CustomHelper/CustomHelper/GRPC/TestHelpers/TestServerStreamWriter.cs
@@ -0,0 +1,59 @@
+﻿using Grpc.Core;
+using System.Threading.Channels;
+
+namespace CustomHelper.GRPC.TestHelpers
+{
+    //all data for mock test get from https://github.com/dotnet/AspNetCore.Docs/blob/main/aspnetcore/grpc/test-services/sample/Tests/Server/UnitTests/Helpers/TestServerStreamWriter.cs
+    public class TestServerStreamWriter<T> : IServerStreamWriter<T> where T : class
+    {
+        private readonly ServerCallContext _serverCallContext;
+        private readonly Channel<T> _channel;
+
+        public WriteOptions? WriteOptions { get; set; }
+
+        public TestServerStreamWriter(ServerCallContext serverCallContext)
+        {
+            _channel = Channel.CreateUnbounded<T>();
+
+            _serverCallContext = serverCallContext;
+        }
+
+        public void Complete()
+        {
+            _channel.Writer.Complete();
+        }
+
+        public IAsyncEnumerable<T> ReadAllAsync()
+        {
+            return _channel.Reader.ReadAllAsync();
+        }
+
+        public async Task<T?> ReadNextAsync()
+        {
+            if (await _channel.Reader.WaitToReadAsync())
+            {
+                _channel.Reader.TryRead(out var message);
+                return message;
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        public Task WriteAsync(T message)
+        {
+            if (_serverCallContext.CancellationToken.IsCancellationRequested)
+            {
+                return Task.FromCanceled(_serverCallContext.CancellationToken);
+            }
+
+            if (!_channel.Writer.TryWrite(message))
+            {
+                throw new InvalidOperationException("Unable to write message.");
+            }
+
+            return Task.CompletedTask;
+        }
+    }
+}
