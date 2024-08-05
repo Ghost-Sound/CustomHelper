666bbc13b5365a8913c0e3e31361ae4a6be3a619
Yehor Kadeniuk <91326774+newmasterSG@users.noreply.github.com>
2024-08-05 22:04:41 +0300

    add helper for test context

---
+++ CustomHelper/CustomHelper/EFcore/TestHelpers/DbSetMockHelper.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Moq;
+using System.Data.Entity.Infrastructure;
+using System.Reflection.Metadata;
+namespace CustomHelper.EFcore.TestHelpers
+{
+    public static class DbSetMockHelper
+    {
+        public static Mock<DbSet<T>> CreateMockDbSet<T>(IQueryable<T> data) where T : class
+        {
+            var mockSet = new Mock<DbSet<T>>();
+            mockSet.As<IQueryable<T>>().Setup(m => m.Provider).Returns(data.Provider);
+            mockSet.As<IQueryable<T>>().Setup(m => m.Expression).Returns(data.Expression);
+            mockSet.As<IQueryable<T>>().Setup(m => m.ElementType).Returns(data.ElementType);
+            mockSet.As<IQueryable<T>>().Setup(m => m.GetEnumerator()).Returns(data.GetEnumerator());
+
+            return mockSet;
+        }
+
+        public static Mock<DbSet<T>> CreateMockDbSetAsync<T>(IQueryable<T> data) where T : class
+        {
+            var mockSet = new Mock<DbSet<T>>();
+
+            mockSet.As<IDbAsyncEnumerable<T>>()
+                    .Setup(m => m.GetAsyncEnumerator())
+                    .Returns(new TestDbAsyncEnumerator<T>(data.GetEnumerator()));
+
+            mockSet.As<IQueryable<T>>()
+            .Setup(m => m.Provider)
+                .Returns(new TestDbAsyncQueryProvider<T>(data.Provider));
+
+            mockSet.As<IQueryable<T>>().Setup(m => m.Expression).Returns(data.Expression);
+            mockSet.As<IQueryable<T>>().Setup(m => m.ElementType).Returns(data.ElementType);
+            mockSet.As<IQueryable<T>>().Setup(m => m.GetEnumerator()).Returns(data.GetEnumerator());
+
+            return mockSet;
+        }
+    }
+}
