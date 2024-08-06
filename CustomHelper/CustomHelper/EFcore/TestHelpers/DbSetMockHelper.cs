4467b5c82c52bde700204943cdf27fc8aad909a4
Yehor Kadeniuk <91326774+newmasterSG@users.noreply.github.com>
2024-08-06 21:19:56 +0300

    update helper

--- CustomHelper/CustomHelper/EFcore/TestHelpers/DbSetMockHelper.cs
+++ CustomHelper/CustomHelper/EFcore/TestHelpers/DbSetMockHelper.cs
@@ -6,32 +6,36 @@ namespace CustomHelper.EFcore.TestHelpers
 {
     public static class DbSetMockHelper
     {
-        public static Mock<DbSet<T>> CreateMockDbSet<T>(IQueryable<T> data) where T : class
+        public static Mock<DbSet<T>> CreateMockDbSet<T>(IEnumerable<T> data) where T : class
         {
+            var queryable = data.AsQueryable();
+
             var mockSet = new Mock<DbSet<T>>();
-            mockSet.As<IQueryable<T>>().Setup(m => m.Provider).Returns(data.Provider);
-            mockSet.As<IQueryable<T>>().Setup(m => m.Expression).Returns(data.Expression);
-            mockSet.As<IQueryable<T>>().Setup(m => m.ElementType).Returns(data.ElementType);
-            mockSet.As<IQueryable<T>>().Setup(m => m.GetEnumerator()).Returns(data.GetEnumerator());
+            mockSet.As<IQueryable<T>>().Setup(m => m.Provider).Returns(queryable.Provider);
+            mockSet.As<IQueryable<T>>().Setup(m => m.Expression).Returns(queryable.Expression);
+            mockSet.As<IQueryable<T>>().Setup(m => m.ElementType).Returns(queryable.ElementType);
+            mockSet.As<IQueryable<T>>().Setup(m => m.GetEnumerator()).Returns(queryable.GetEnumerator());
 
             return mockSet;
         }
 
-        public static Mock<DbSet<T>> CreateMockDbSetAsync<T>(IQueryable<T> data) where T : class
+        public static Mock<DbSet<T>> CreateMockDbSetAsync<T>(IEnumerable<T> data) where T : class
         {
+            var queryable = data.AsQueryable();
+
             var mockSet = new Mock<DbSet<T>>();
 
             mockSet.As<IDbAsyncEnumerable<T>>()
                     .Setup(m => m.GetAsyncEnumerator())
-                    .Returns(new TestDbAsyncEnumerator<T>(data.GetEnumerator()));
+                    .Returns(new TestDbAsyncEnumerator<T>(queryable.GetEnumerator()));
 
             mockSet.As<IQueryable<T>>()
             .Setup(m => m.Provider)
-                .Returns(new TestDbAsyncQueryProvider<T>(data.Provider));
+                .Returns(new TestDbAsyncQueryProvider<T>(queryable.Provider));
 
-            mockSet.As<IQueryable<T>>().Setup(m => m.Expression).Returns(data.Expression);
-            mockSet.As<IQueryable<T>>().Setup(m => m.ElementType).Returns(data.ElementType);
-            mockSet.As<IQueryable<T>>().Setup(m => m.GetEnumerator()).Returns(data.GetEnumerator());
+            mockSet.As<IQueryable<T>>().Setup(m => m.Expression).Returns(queryable.Expression);
+            mockSet.As<IQueryable<T>>().Setup(m => m.ElementType).Returns(queryable.ElementType);
+            mockSet.As<IQueryable<T>>().Setup(m => m.GetEnumerator()).Returns(queryable.GetEnumerator());
 
             return mockSet;
         }
