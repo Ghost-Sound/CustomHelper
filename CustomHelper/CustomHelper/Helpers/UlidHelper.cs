891b1e9f877165c65a05d5503ddf392258f3c6e2
Yehor Kadeniuk <91326774+newmasterSG@users.noreply.github.com>
2024-08-09 21:33:23 +0300

    add new to helper

---
+++ CustomHelper/CustomHelper/Helpers/UlidHelper.cs
@@ -0,0 +1,16 @@
+﻿namespace CustomHelper.Helpers
+{
+    public static class UlidHelper
+    {
+        public static Ulid ParseUlid(this string id)
+        {
+            if (string.IsNullOrEmpty(id))
+            {
+                return Ulid.NewUlid();
+            }
+
+            _ = Ulid.TryParse(id, out Ulid res);
+            return res;
+        }
+    }
+}
---
+++ CustomHelper/CustomHelper/PipelineBehavior/CacheBehavior.cs
@@ -0,0 +1,41 @@
+﻿using MediatR;
+using Microsoft.Extensions.Caching.Distributed;
+using System.Text.Json;
+
+namespace CustomHelper.PipelineBehavior
+{
+    public class CacheBehavior<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse>
+        where TRequest : IRequest<TResponse>
+    {
+        private readonly IDistributedCache _cache;
+
+        public CacheBehavior(IDistributedCache cache) => _cache = cache;
+
+        public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
+        {
+            var key = $"{typeof(TRequest).FullName}:{GetRequestHashCode(request)}";
+
+            var cachedResponse = await _cache.GetStringAsync(key);
+            if (cachedResponse != null)
+            {
+                return JsonSerializer.Deserialize<TResponse>(cachedResponse);
+            }
+
+            var response = await next();
+            var options = new DistributedCacheEntryOptions
+            {
+                AbsoluteExpirationRelativeToNow = TimeSpan.FromMinutes(5),
+                SlidingExpiration = TimeSpan.FromMinutes(1),
+            };
+            var json = JsonSerializer.Serialize(response);
+            await _cache.SetStringAsync(key, json, options);
+
+            return response;
+        }
+
+        private string GetRequestHashCode(TRequest request)
+        {
+            return JsonSerializer.Serialize(request).GetHashCode().ToString();
+        }
+    }
+}
